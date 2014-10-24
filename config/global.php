196c078fef08c25d6c8955e3f1bfeba6c7418484
jennifergarner <jennifergarner@mail.fresnostate.edu>
2014-10-23 21:54:11 -0700

    diagnostics

--- config/global.php
+++ config/global.php
@@ -3,23 +3,24 @@
     global $rootdir;
     $rootdir = "../";
     //$rootdir = "/Users/stell_000/Documents/GitHub/ForAaronsResearch/";
-    
+    echo "subdir";
     global $subdir;
     //$subdir = "/ForAaronsResearch/";
-    
+    echo "header";
     global $header;
     $header = $rootdir."config/header.php";
-    
+    echo "footer";
     global $footer;
     $footer = $rootdir."config/footer.php";
-
+    echo "functions";
     global $functions;
     $functions = $rootdir."functions/functions.php";
     require_once ($functions);
-    
+    echo "modules";
     global $modules;
     $modules = $rootdir."config/modules.php";
     require_once ($modules);
-    
+    echo "date";
     date_default_timezone_set("America/Los_Angeles");
+    echo "end";
 ?>
