3a2645bc556edffae228471db5a719d3cf8215b7
jennifergarner <jennifergarner@mail.fresnostate.edu>
2014-10-23 23:10:22 -0700

    asdf

--- test/imageTest.php
+++ test/imageTest.php
@@ -11,9 +11,7 @@
     if (empty($_POST["name"]) and empty($_GET)) : ?>
     <div id="start">
 <?php 
-    var_dump (file_get_contents("../test/image_tests.json"));
-    echo "inbetween";
-    var_dump (file_get_contents("/test/image_tests.json"));
+    var_dump (file_get_contents("image_tests.json"));
     $test = decodeJSON ("image_tests.json");    
     if (empty($test)) :
         echo "<h2>Error - no tests available.</h2>";
