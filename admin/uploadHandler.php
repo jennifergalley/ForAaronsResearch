326026bd6efd53f940b89f6ff3a575ada45e137f
jennifergarner <jennifergarner@mail.fresnostate.edu>
2014-10-25 14:39:02 -0700

    diagnostics

--- admin/uploadHandler.php
+++ admin/uploadHandler.php
@@ -1,13 +1,19 @@
 <?php
+    echo "hello world<br>";
     $num = count($_FILES);
     for ($i = 0; $i < $num; $i++) {
+        echo $i."<br>";
         $fileName = 'gs://aarons-tests/images/'.$_FILES['file']['name'][$i];
         $tmpname = 'gs://aarons-tests/images/'.$_FILES['file']['tmp_name'][$i];
+        echo $fileName."<br>";
+        echo $tmpname."<br>";
         
         $options = array('gs'=>array('Content-Type' => $_FILES['file']['type'][$i]));
         $ctx = stream_context_create($options);
+        echo $options."<br>";
+        echo $ctx."<br>";
         
-        if (false === rename($_FILES['file']['tmp_name'][$i], $fileName, $ctx)) { //duplicate
+        if (false == rename($_FILES['file']['tmp_name'][$i], $fileName, $ctx)) { //duplicate
             echo "deleting file";
             echo unlink($_FILES['file']['tmp_name'][$i]); //delete
         }
