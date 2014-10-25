6694e55fa9d964d4008e0da25f52cea2e00db546
jennifergarner <jennifergarner@mail.fresnostate.edu>
2014-10-25 13:31:43 -0700

    testing b

--- admin/testingBrowse.php
+++ admin/testingBrowse.php
@@ -1,11 +1,14 @@
 <?php
-    require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
-    use google\appengine\api\cloud_storage\CloudStorageTools;
+    if ($handle = opendir('gs://aarons-tests/images')) {
+        echo "Directory handle: $handle\n";
+        echo "Entries:\n";
     
-    $object_url = 'gs://aarons-tests/images/L2FwcGhvc3RpbmdfcHJvZC9ibG9icy9BRW5CMlVxTDJfcmVobTRuN29FOGNxWjhBd0tjT3ZSVkFEb2xsR1hobUR2V0g3VGNJempoOFMyZGttc3k3YVBBZ01laVdhdXAxSkFhMTRvVWE3czdRMmhRS2toOGxobGtEdy5GSXZ2UFNjR3htOFFjaDhm.png';
-    //$options = stream_context_create(array('gs'=>array('acl'=>'public-read')));
+        /* This is the correct way to loop over the directory. */
+        while (false !== ($entry = readdir($handle))) {
+            echo "$entry\n";
+            echo "<a href='http://storage.googleapis.com/aarons-tests/images/$entry' target='_blank'>Click Me</a>\n";
+        }
     
-    $object_public_url = CloudStorageTools::getPublicUrl($object_url, false);
-    
-    header('Location:' .$object_public_url);
+        closedir($handle);
+    }
 ?>
