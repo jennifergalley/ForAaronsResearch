2eb03f9978929e9027839f203fb4c970f26cfd7f
jennifergarner <jennifergarner@mail.fresnostate.edu>
2014-10-25 13:36:23 -0700

    asdf

--- admin/testingBrowse.php
+++ admin/testingBrowse.php
@@ -1,5 +1,6 @@
 <?php
-    if ($handle = opendir('gs://aarons-tests/images')) {
+    echo "hello world";
+    if ($handle = opendir('gs://aarons-tests/images/')) {
         echo "Directory handle: $handle\n";
         echo "Entries:\n";
     
@@ -11,4 +12,11 @@
     
         closedir($handle);
     }
+    
+    require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
+    use google\appengine\api\cloud_storage\CloudStorageTools;
+    
+    $object_url = 'gs://aarons-tests/images/L2FwcGhvc3RpbmdfcHJvZC9ibG9icy9BRW5CMlVxTDJfcmVobTRuN29FOGNxWjhBd0tjT3ZSVkFEb2xsR1hobUR2V0g3VGNJempoOFMyZGttc3k3YVBBZ01laVdhdXAxSkFhMTRvVWE3czdRMmhRS2toOGxobGtEdy5GSXZ2UFNjR3htOFFjaDhm.png';
+    $object_public_url = CloudStorageTools::getPublicUrl($object_url, false);
+    echo $object_public_url;    
 ?>
