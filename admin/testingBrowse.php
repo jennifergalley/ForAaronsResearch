03a9533b376b02344c22c7e3dec0de076cccd017
jennifergarner <jennifergarner@mail.fresnostate.edu>
2014-10-25 14:15:18 -0700

    !!!!

--- admin/testingBrowse.php
+++ admin/testingBrowse.php
@@ -1,8 +1,7 @@
 <?php
     if ($handle = opendir('gs://aarons-tests/images/')) {
         while (false !== ($entry = readdir($handle))) {
-            //echo "$entry\n"; //file name
-            echo "<img src='http://storage.googleapis.com/aarons-tests/images/$entry'>\n";
+            echo "<img src='http://storage.googleapis.com/aarons-tests/images/$entry' style='max-height:100px;max-width:100px'>\n";
         }
         closedir($handle);
     }
--- admin/uploadHandler.php
+++ admin/uploadHandler.php
@@ -1,21 +1,15 @@
 <?php
-    var_dump ($_FILES);
-    
     $fileName = 'gs://aarons-tests/images/'.$_FILES['file']['name'][0];
-    echo $fileName."<br>";
     
-    $options = array('gs'=>array('acl'=>'public-read','Content-Type' => $_FILES['file']['type'][0]));
+    $options = array('gs'=>array('Content-Type' => $_FILES['file']['type'][0]));
     $ctx = stream_context_create($options);
     
     if (false == rename($_FILES['file']['tmp_name'][0], $fileName, $ctx)) {
       die('Could not rename.');
     }
-    
-    $object_public_url = CloudStorageTools::getPublicUrl($fileName, true);
-    echo $objectPublicUrl."<br>";
 ?>
 <script type="text/javascript">
-    //window.location = "upload.php";
+    window.location = "upload.php";
 </script>
 
 
