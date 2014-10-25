7303ad0e10026202b291ea78453b586c5eb837da
jennifergarner <jennifergarner@mail.fresnostate.edu>
2014-10-25 14:48:05 -0700

    asdf

--- admin/uploadHandler.php
+++ admin/uploadHandler.php
@@ -4,7 +4,7 @@
         $fileName = 'gs://aarons-tests/images/'.$_FILES['file']['name'][$i];
         $tmpname = 'gs://aarons-tests/images/'.$_FILES['file']['tmp_name'][$i];
         
-        $options = array('gs'=>array('Content-Type' => $_FILES['file']['type'][$i]));
+        $options = array('gs'=>array('acl'=>'public-read', 'Content-Type' => $_FILES['file']['type'][$i]));
         $ctx = stream_context_create($options);
         
         if (false == rename($_FILES['file']['tmp_name'][$i], $fileName, $ctx)) { //duplicate
