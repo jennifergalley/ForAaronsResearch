8cda714939ed4451d7fdc073077801e62899fc98
jennifergarner <jennifergarner@mail.fresnostate.edu>
2014-11-02 17:07:20 -0800

    asdf

--- js/image_test.js
+++ js/image_test.js
@@ -78,6 +78,7 @@
                 var url = "../results/saveImageResponses.php?participant="+participant+"&testVersion="+testVersion+"&";
                 var f = 1;
                 for (k=1; k <= blocks; k++) {
+                    alert (k);
                     for (h=1; h <= numberQuestions[k]; h++) {
                         url += f+"="+getCookie("response."+k+"."+h)+"&";
                         f++;
